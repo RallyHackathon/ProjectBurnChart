a554c26b2c8995096b72284eb795751d5d0d9d7e
Rally Dev Pair <pairing@rallydev.com>
2013-05-31 11:52:12 -0600

    Added basic stacked-column with hard-coded data.

--- App.js
+++ App.js
@@ -1,8 +1,65 @@
 Ext.define('CustomApp', {
-    extend: 'Rally.app.App',
-    componentCls: 'app',
+	extend: 'Rally.app.App',
+	componentCls: 'app',
 
-    launch: function() {
+	items: [
+		{
+			xtype: 'rallychart',
+			chartData: {
+				series: [{
+					name: 'John',
+					data: [5, 3, 4, 7, 2]
+				}, {
+					name: 'Jane',
+					data: [2, 2, 3, 2, 1]
+				}, {
+					name: 'Joe',
+					data: [3, 4, 4, 2, 5]
+				}]
+			},
+			chartConfig: {
+				chart: {
+					type: 'column'
+				},
+				title: {
+					text: 'Stacked column chart'
+				},
+				xAxis: {
+					categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+				},
+				yAxis: {
+					min: 0,
+					title: {
+						text: 'Total fruit consumption'
+					}
+				},
+				legend: {
+					align: 'right',
+					x: -100,
+					verticalAlign: 'top',
+					y: 20,
+					floating: true,
+					borderColor: '#CCC',
+					borderWidth: 1,
+					shadow: false
+				},
+				tooltip: {
+					formatter: function() {
+						return '<b>'+ this.x +'</b><br/>'+
+						this.series.name +': '+ this.y +'<br/>'+
+						'Total: '+ this.point.stackTotal;
+					}
+				},
+				plotOptions: {
+					column: {
+						stacking: 'normal'
+					}
+				}
+			}
+		}
+	],
+
+	launch: function() {
         //Write app code here
     }
 });
